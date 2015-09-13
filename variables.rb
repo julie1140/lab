f56bfb7145aa60549341b2f54f9dc5381c69c2a4
Julie Suh <julie1140@gmail.com>
2015-09-12 17:07:24 -0700

    Added lab stuff from Day 1

---
+++ variables.rb
@@ -0,0 +1,6 @@
+sentence1="This is repetitive..."
+sentence2="...I'm glad I'm lazy!"
+
+puts sentence1*2
+puts sentence2*2
+puts sentence1
---
+++ who_am_i.rb
@@ -0,0 +1,13 @@
+puts "What is your full name?"
+name=gets.chomp.downcase
+
+puts "Now where did you grow up?"
+growup=gets.chomp.downcase
+
+puts "What is your current occupation?"
+occupation=gets.chomp.downcase
+
+puts "What is your hidden talent?"
+talent=gets.chomp.downcase
+
+puts "Your name is #{name}, you are from #{growup}, your occupation is #{occupation}, and your hidden talent is #{talent}."
